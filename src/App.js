f26aa1e280dae8d4f06ec6a99663081d16468257
joshua <joshua@stormserver.nohost.me>
2023-03-28 15:36:07 +0100

    use template

--- src/App.js
+++ src/App.js
@@ -1,16 +1,74 @@
-import * as React from 'react';
-import Button from '@mui/joy/Button';
-import Box from '@mui/joy/Box';
-import Badge from '@mui/joy/Badge';
+import * as React from "react";
+import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
+import Sheet from "@mui/joy/Sheet";
+import Typography from "@mui/joy/Typography";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
+import Link from "@mui/joy/Link";
 
-export default function BasicButtons() {
-  return (    
-    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', backgroundColor: 'rgba(255 255 255 / 0.6)',}} >
-      <Badge badgeContent="Nextcloud" >
-        <Button img="https://nextcloud.com/wp-content/uploads/2022/05/nextcloud-logo.png">
-        </Button>
-      </Badge>
-        
-    </Box>
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  // necessary for server-side rendering
+  // because mode is undefined on the server
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
+
+function visitnextcloud() {
+  window.open("http://localhost:8081");
+  return;
+}
+
+export default function App() {
+  return (
+    <CssVarsProvider>
+      <main>
+        <ModeToggle />
+        <Sheet
+          sx={{
+            width: 300,
+            mx: "auto", // margin left & right
+            my: 4, // margin top & botom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            borderRadius: "sm",
+            boxShadow: "md"
+          }}
+          variant="outlined"
+        >
+          <div>
+            <Typography level="h4" component="h1">
+              <b>Welcome, Choose An App</b>
+            </Typography>
+            <Typography level="body2"></Typography>
+          </div>
+          <Button sx={{ mt: 1 /* margin top */ }} onClick={visitnextcloud}>
+            Nextcloud
+          </Button>
+        </Sheet>
+      </main>
+    </CssVarsProvider>
   );
 }

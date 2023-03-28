626aeef78e93cb900b8992024d9b3c0277a218a3
joshua <joshua@stormserver.nohost.me>
2023-03-28 14:51:08 +0100

    Add Nextcloud Button, First Steps!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Button from '@mui/joy/Button';
 import Box from '@mui/joy/Box';
+import Badge from '@mui/joy/Badge';
 
 export default function BasicButtons() {
   return (    
     <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', backgroundColor: 'rgba(255 255 255 / 0.6)',}} >
-      <Button>Button</Button>
-      <Button disabled>Disabled</Button>
-      <Button loading>Loading</Button>
+      <Badge badgeContent="Nextcloud" >
+        <Button img="https://nextcloud.com/wp-content/uploads/2022/11/nextcloud-logo.svg">
+        </Button>
+      </Badge>
+        
     </Box>
   );
 }

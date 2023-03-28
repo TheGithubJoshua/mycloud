0e43b4b64cb7d0657123f7a5052bf7a0c43681e1
joshua <joshua@stormserver.nohost.me>
2023-03-28 14:54:31 +0100

    hihihihihikkkhik

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function BasicButtons() {
   return (    
     <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', backgroundColor: 'rgba(255 255 255 / 0.6)',}} >
       <Badge badgeContent="Nextcloud" >
-        <Button img="https://nextcloud.com/wp-content/uploads/2022/11/nextcloud-logo.svg">
+        <Button img="https://nextcloud.com/wp-content/uploads/2022/05/nextcloud-logo.png?original">
         </Button>
       </Badge>
         
